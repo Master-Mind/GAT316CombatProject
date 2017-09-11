4b7f192503d5f77600d6275c098bdbe8dfc6ad76
philip.a.hollingsworth@gmail.com <philip.a.hollingsworth@gmail.com>
2017-09-11 13:23:54 -0700

    More progress on weapons

---
+++ Assets/Scripts/ActionSystem/WaitAction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.ActionSystem;
+
+public class WaitAction : Action
+{
+    private float time = 0;
+    private float endTime = 0;
+
+
+    public override bool Execute()
+    {
+        time += Time.deltaTime;
+        return time >= endTime;
+    }
+
+    public WaitAction(GameObject objectToActOn, float endTime) : base(objectToActOn)
+    {
+        this.endTime = endTime;
+    }
+}
--- Assets/Weapons/Weapon.cs
+++ Assets/Weapons/Weapon.cs
@@ -1,28 +1,41 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.ActionSystem;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
     public Vector3 RestingPos;
+    public Quaternion RestingRot;
     private ActionSystem _actions;
-    public
+    private Action[] QuickMoveset;
+    private bool _isResting = false;
 	// Use this for initialization
 	void Start ()
     {
         transform.localPosition = RestingPos;
+        RestingRot = transform.localRotation;
         _actions = GetComponent<ActionSystem>();
+        _isResting = true;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(!_actions.IsActive && !_isResting)
+        {
+            ArrayList group = new ArrayList();
+            group.Add(new SlerpRotAction(gameObject, RestingRot, 0.1f));
+            group.Add(new InterpolateAction(gameObject, RestingPos, 0.1f));
+            _actions.AddAction(new ActionSequence(gameObject, group));
+            _isResting = true;
+        }
 	}
 
     public void QuickAttack()
     {
         if (!_actions.IsActive)
         {
+            _isResting = false;
             ArrayList group = new ArrayList();
             ArrayList seq = new ArrayList();
             int scaler = 1;
@@ -35,7 +48,7 @@ public class Weapon : MonoBehaviour
             group.Add(new InterpolateAction(gameObject, Vector3.right * scaler, 0.1f));
             seq.Add(new ActionGroup(gameObject, group));
             seq.Add(new SlerpAboutAction(gameObject, gameObject, 0.2f, (-180) * scaler));
-
+            seq.Add(new WaitAction(gameObject, 0.5f));
 
             _actions.AddAction(new ActionSequence(gameObject, seq));
         }
